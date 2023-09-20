f906507d07266d621ede4571977cdbe337024633
Eric Bouck <eric.bouck@gmail.com>
2023-09-20 15:00:49 -0700

    Get rid of github-linear and github-slack for now

--- github-linear/on-webhook-update-linear-state/index.js
+++
@@ -1,8 +0,0 @@
-import { handler, getDeployList } from "@runlightyear/lightyear";
-import "./src";
-
-exports.handler = handler;
-global.handler = handler;
-
-exports.getDeployList = getDeployList;
-global.getDeployList = getDeployList;
