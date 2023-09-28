191b4563956755780d1dc8d1a8af82ec94f3be30
Eric Bouck <eric.bouck@gmail.com>
2023-09-27 18:28:29 -0700

    Add descriptions to compareTwoCommits variables

--- github/src/compareTwoCommits.ts
+++ github/src/compareTwoCommits.ts
@@ -5,7 +5,23 @@ defineAction({
   name: "compareTwoCommits",
   title: "Compare Two Commits",
   apps: ["github"],
-  variables: ["owner", "repo", "basehead"],
+  variables: [
+    {
+      name: "owner",
+      description:
+        "The account owner of the repository. The name is not case sensitive.",
+    },
+    {
+      name: "repo",
+      description:
+        "The name of the repository. The name is not case sensitive.",
+    },
+    {
+      name: "basehead",
+      description:
+        "The base branch and head branch to compare. This parameter expects the format BASE...HEAD. Both must be branch names in repo. To compare with a branch that exists in a different repository in the same network as repo, the basehead parameter expects the format USERNAME:BASE...USERNAME:HEAD.",
+    },
+  ],
   run: async ({ auths, variables }) => {
     const github = new GitHub({
       auth: auths.github,
